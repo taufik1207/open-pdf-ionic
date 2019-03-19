270ed887f67189e2a88733343f05d04c4e3351c7
taufik1207 <taufik1207@gmail.com>
2019-03-19 16:48:53 +0700

    Add example code

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { FileTransfer, FileTransferObject } from '@ionic-native/file-transfer';
+import { File } from '@ionic-native/file';
+import { AndroidPermissions } from '@ionic-native/android-permissions';
+import { FileOpener } from '@ionic-native/file-opener';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +11,32 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public transfer: FileTransfer, public file: File, public androidPermissions: AndroidPermissions, public fileOpener: FileOpener) {
 
+  }
+
+  download() {
+    this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.WRITE_EXTERNAL_STORAGE).then(
+      (result => {
+        if(result.hasPermission == false) {
+          this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.WRITE_EXTERNAL_STORAGE)
+        } else {
+          const fileTransfer: FileTransferObject = this.transfer.create();
+
+          const uri = "https://object.mycoop.id/middleware-mobile-pulsa/999-BPJS-1552715528738.pdf";
+          fileTransfer.download(uri, this.file.dataDirectory + "bukti.pdf").then((res) => {
+            console.log('download completed: ' + res.toURL()); 
+
+            this.fileOpener.open(res.toURL(), 'application/pdf')
+            .then(() => console.log('File is opened'))
+            .catch(e => console.log('Error opening file', e));
+          }, (error) => {
+            console.log("download failed: " + error)
+          })
+        }
+      }),
+      err => this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.WRITE_EXTERNAL_STORAGE)
+    );
   }
 
 }
